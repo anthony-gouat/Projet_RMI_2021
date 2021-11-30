e4c7f65cc7847e99d23559a14a6dbf0c028d258d
Maxime MEYER <meyer.maxime212@gmail.com>
2021-11-30 11:54:17 +0100

    interface

--- client/src/main/java/client/Connexion.java
+++ client/src/main/java/client/Connexion.java
@@ -1,4 +1,23 @@
 package client;
 
-public class Connexion {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Connexion extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("connexion.fxml"));
+//        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("C:\\Users\\antho\\Documents\\Licence\\app répartie\\Projet_RMI_2021\\client\\src\\main\\resources\\hello-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
 }
